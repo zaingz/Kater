4f23ee5cc10c86ba484a4b6beb411a73bbaa871d
zaingz <zaing.143@gmail.com>
2015-07-17 12:05:02 +0500

    cart experiment, many to many final done

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,4 +1,7 @@
 class FoodItem < ActiveRecord::Base
+
+
+
 	attr_accessor :catering_company_id, :deal_id, :food_item_add_on_id
 
 	belongs_to :catering_company
--- db/migrate/20150715080210_create_deals.rb
+++ db/migrate/20150715080210_create_deals.rb
@@ -2,10 +2,12 @@ class CreateDeals < ActiveRecord::Migration
   def change
     create_table :deals do |t|
 
+
       t.string :name, null: false, default: ""
       t.text :description, null: false, default: ""
       t.float :price, null: false
       t.references :catering_company
+
 
 
       t.timestamps null: false
