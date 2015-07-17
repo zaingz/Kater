bbbae8adac3d96c88ca3931edab668f757dcc76a
haseebahmad109 <haseebahmad109@gmail.com>
2015-07-17 14:59:13 +0500

    users count in main dashboard admin

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
   before_action :authorize_as_super_admin, only: [:super_admin]
 
   def super_admin
+
+  	@users = User.all
+  	@c_company = CateringCompany.all
+
   end
 
   def manager_admin
