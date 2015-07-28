1b54d1e7a75c8684e4c736949aacd43695cd7241
Haseeb Ahmad <haseebahmad109@gmail.com>
2015-07-28 16:42:56 +0500

    quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,9 +3,9 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
 
 
-  has_many :order_item_foods
+  has_many :oder_item_foods
   has_many :food_items, through: :oder_item_foods
-  has_many :order_item_deals
+  has_many :oder_item_deals
   has_many :deals, through: :oder_item_deals
 
 end
